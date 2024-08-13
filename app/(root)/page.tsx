72ecdf0735393a354669602a072a538820f77792
Kisara Batugedara <kbat0008@student.monash.edu>
2024-08-13 11:12:13 +1000

    Implement doughnut chart and set home page UI.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Kisara'};
+
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type ="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage  your transactions and account efficiently"
+                />
+
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1500.89}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
