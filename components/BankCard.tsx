6eed1d5e978329ec3227d3c11ebeab2528a76915
Kisara Batugedara <kisara.batugedara1@gmail.com>
2024-08-22 08:22:05 +1000

    Fix censoring of sensitive data on user bank card.

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -38,7 +38,7 @@ const BankCard = ({account, userName, showBalance=true}: CreditCardProps) => {
                     </div>
                     <p className='text-14 font-semibold tracking-[1.1px] text-white'>
 
-                        ●●●● ●●●● ●●●● <span className='text-16'> 2345 </span>
+                            ●●●● ●●●● ●●●● <span className='text-16'> 2345 </span>
 
                     </p>
                 </article>
